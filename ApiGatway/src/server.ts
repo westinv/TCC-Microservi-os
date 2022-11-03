f2708ffe7155c13060f86f6822f5c68e0757b168
westinv <43423127+westinv@users.noreply.github.com>
2022-11-03 16:37:01 -0300

    Feat: ApiGatway

---
+++ ApiGatway/src/server.ts
@@ -0,0 +1,18 @@
+import express, { request, response } from "express"
+import helmet from 'helmet'
+import httpProxy from 'express-http-proxy'
+
+
+const app = express()
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("", httpProxy("http://localhost:3333/user", { timeout: 3000 }))
+
+app.get('/', (request, response) => {
+    return response.json({ message: "oi" })
+})
+
+
+
+app.listen(3334, () => console.log("server on"))// mudar pois bate com a auth 3335 para o gatway
